f78c1e47be03fe27a1da083b5cb22ee41f8436c3
piopoi <kangsw1988@gmail.com>
2024-04-17 12:15:46 +0900

    init project

---
+++ src/main/java/com/piopoi/blogbasic/BlogBasicApplication.java
@@ -0,0 +1,13 @@
+package com.piopoi.blogbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogBasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/piopoi/blogbasic/BlogBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piopoi.blogbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
